9a254d2bbf84409e375497b5405a3a89b478407f
Lucianoschmerega <163128607+Lucianoschmerega@users.noreply.github.com>
2024-10-22 21:17:49 -0300

    Ex2.js

---
+++ Ex2.js
@@ -0,0 +1,14 @@
+let A, B, C
+
+A = prompt("Digite o primeiro valor: ")
+B = prompt("Digite o segundo valor: ")
+C = prompt("Digite o teceiro valor: ")
+
+if(A>B && A>C)
+console.log("O maior Valor foi a primeira: " + A)
+
+if(B>A && B>C)
+console.log("O maior valor foi a segunda: " + B)
+
+if(C>B && C>A)
+console.log("O maior valor foi a terceira: " + C)
