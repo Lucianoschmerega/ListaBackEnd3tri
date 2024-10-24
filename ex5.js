b7a4cc2539230a9ee1a9b4be1ce738ecd34355a2
Lucianoschmerega <163128607+Lucianoschmerega@users.noreply.github.com>
2024-10-24 08:09:09 -0300

    Create ex5.js

---
+++ ex5.js
@@ -0,0 +1,14 @@
+let A, B, C
+
+A = prompt("Digite o primeiro valor: ")
+B = prompt("Digite o segundo valor: ")
+C = prompt("Digite o teceiro valor: ")
+
+if(A>B && A>C)
+console.log(A)
+
+if(B>A && B>C)
+console.log(B)
+
+if(C>B && C>A)
+console.log(C)
