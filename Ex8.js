a4c492126f27db7c9ab9958dff6902dd12ac5297
Lucianoschmerega <163128607+Lucianoschmerega@users.noreply.github.com>
2024-10-24 10:39:29 -0300

    Create Ex8.js

---
+++ Ex8.js
@@ -0,0 +1,21 @@
+let A
+
+A = prompt("Digite o numero do pedido: ")
+
+if(A == 100)
+console.log("o seu pedido ficou em 1,20")
+
+if(A == 101)
+console.log("o seu pedido ficou em 1,30")
+
+if(A == 102)
+console.log("o seu pedido ficou em 1,50")
+
+if(A == 103)
+console.log("o seu pedido ficou em 1,20")
+
+if(A == 104)
+console.log("o seu pedido ficou em 1,30")
+
+if(A == 105)
+console.log("o seu pedido ficou em 1,00")
