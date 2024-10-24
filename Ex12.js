aaa92afb9f75a9a1c22117be150e9e83e940a9e5
Lucianoschmerega <163128607+Lucianoschmerega@users.noreply.github.com>
2024-10-24 10:59:03 -0300

    Create Ex12.js

---
+++ Ex12.js
@@ -0,0 +1,21 @@
+let A,B,C
+
+A = prompt("Qual é o codigo do produto? ")
+B = prompt("Quantas unidades deseja comprar? ")
+
+if(A == 1001)
+C = B * 5.32
+
+if(A == 1324)
+C = B * 6.45
+
+if(A == 6548)
+C = B * 2.37
+
+if(A == 0987)
+C = B * 5.32
+
+if(A == 7623)
+C = B * 6.45
+
+console.log("o total da comprar foi de: "+ C)
