80319ad67212e4674564fb4c926c26e4b4c2cfe1
Lucianoschmerega <163128607+Lucianoschmerega@users.noreply.github.com>
2024-10-24 08:04:12 -0300

    Create Ex3.js

---
+++ Ex3.js
@@ -0,0 +1,10 @@
+let A, B,
+
+A = prompt("Digite o primeiro numero: ")
+B = prompt("Digite o segundo numero: ")
+
+if(A%B == 0|| B%A == 0)
+console.log("eles sao multiplos")
+
+else 
+console.log("eles n são multiplos")
