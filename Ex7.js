3fa0dee5865930330a4f471c800ee03623ad9204
Lucianoschmerega <163128607+Lucianoschmerega@users.noreply.github.com>
2024-10-24 08:24:14 -0300

    Create Ex7.js

---
+++ Ex7.js
@@ -0,0 +1,14 @@
+let A
+
+A = prompt("Digite o primeiro numero: ")
+
+if(A%2 === 0)
+console.log("o numero é par")
+else
+console.log("o numero é impar")
+
+if(A>=0)
+console.log("o numero é positivo")
+
+else
+console.log("o numero é negativo")
