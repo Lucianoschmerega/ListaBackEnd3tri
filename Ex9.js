296a244b1a3524592f3dea1d65aede0a88d3142b
Lucianoschmerega <163128607+Lucianoschmerega@users.noreply.github.com>
2024-10-24 10:47:51 -0300

    Create Ex9.js

---
+++ Ex9.js
@@ -0,0 +1,12 @@
+let S,H, a
+
+H = prompt("Qual a sua altura? ")
+S = prompt("Qual é seu sexo?(digite somente M para masculino e F para feminino) ")
+
+if(S = "M")
+a= (72.7*H)-58
+
+if(S="F")
+a= (62.1*H)-44.7
+
+console.log("seu peso ideal para a sua altura é: "+a)
